6c8ce8386e9ab3927963d298dd3fcf4407d943b3
Natalia Tajuelo <tajuelonatalia@gmail.com>
2024-10-16 12:24:27 +0000

    hecho

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import seaborn as sns
 from dotenv import load_dotenv
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
+import matplotlib.pyplot as plt
 
 
 # load the .env file variables
@@ -14,12 +15,34 @@ client_secret = os.environ.get("CLIENT_SECRET")
 auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-#artist = 'spotify:artist:64tJ2EAv1R6UaZqc4iOCyj'
+artist_ID = "64tJ2EAv1R6UaZqc4iOCyj"
 
-artist = "64tJ2EAv1R6UaZqc4iOCyj"
-
-result = sp.artist_top_tracks("64tJ2EAv1R6UaZqc4iOCyj")
+result = sp.artist_top_tracks(artist_ID, country="JP")
 if result: 
-    tracks = result["tracks"] 
-    tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v 
-    for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+    tracks = result["tracks"]
+    tracks = [
+        {
+            "name": track["name"],
+            "popularity": track["popularity"],
+            "duration_ms": (track["duration_ms"] / (1000 * 60))  # Convert to minutes
+        }
+        for track in tracks
+    ]
+
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+
+print(tracks_df.head(5))
+
+sns.set_theme(style="whitegrid")
+cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
+
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+
+plt.xlabel('Popularity')
+plt.ylabel('Duration (minutes)')
+plt.title('Relation between Popularity and Duration of Top Tracks')
+plt.savefig("YOASOBI_top_tracks.png")
+
+plt.title('Relation between the Popularity and the Duration')
+plt.show()
